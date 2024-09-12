11f52a0368ad5b28a9d5c2a567a928c296338c2e
chase sunstrom <casunstrom+github@gmail.com>
2024-09-12 13:46:37 -0500

    Add project files.

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+﻿workspace "Bank"
+    architecture "x64"
+    configurations { "Debug", "Release", "Distribution" }
+    startproject "Bank"
+
+    -- Enable multithreaded builds
+    flags { "MultiProcessorCompile" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+filter "configurations:Debug"
+    defines "DEBUG"
+    runtime "Debug"
+    symbols "on"
+
+filter "configurations:Release"
+    defines "NDEBUG"
+    runtime "Release"
+    optimize "on"
+
+filter "configurations:Distribution"
+    defines {"NDEBUG", "__DIST__"}
+    runtime "Release"
+    optimize "Speed"  -- Highest speed optimizations
+    flags { "LinkTimeOptimization" }  -- Enable link-time optimization
+    buildoptions { "/GL", "/Ot" }  -- Favor speed over size (MSVC)
+
+project "Bank"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/%{prj.name}")
+    
+    files {
+        "src/**.hpp",
+        "src/**.cpp"
+    }
