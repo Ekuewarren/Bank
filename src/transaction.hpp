0a745f5427097148da69ac482f6e39d3d793b4f5
Nedmac2112 <168115717+Nedmac2112@users.noreply.github.com>
2024-09-24 12:57:08 -0500

    Updated transaction.hpp

--- src/transaction.hpp
+++ src/transaction.hpp
@@ -8,15 +8,15 @@ namespace Bank
     {
     private:
         i32 m_transaction_id;           // Unique transaction ID
-        BankAccount *m_account;         // Pointer to the account associated with this transaction
+        BankAccount& m_account;         // Reference to the account associated with this transaction
         f64 m_amount;                   // Amount to deposit or withdraw
         std::string m_transaction_type; // Deposit or Withdraw
+        void Execute() const;
 
     public:
         Transaction() = default;
         Transaction(i32 transaction_id, BankAccount *account, f64 amount, const std::string &transaction_type);
 
-        void Execute() const;
         void Display() const;
     };
 }
