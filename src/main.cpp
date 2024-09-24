9a3c9f84887d4cafb2a06048cf0cc2310a0831ea
Nedmac2112 <168115717+Nedmac2112@users.noreply.github.com>
2024-09-24 13:13:12 -0500

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ i32 main()
     Bank::CheckingAccount checking_account(12345, 1000, "John Doe");
 
     std::cout << "Account ID: " << checking_account.GetAccountId() << std::endl;
-    std::cout << "Account Balance: " << checking_account.GetBalance() << std::endl;
+    std::cout << "Account Balance: $" << checking_account.GetBalance() << std::endl;
     std::cout << "Account Holder Name: " << checking_account.GetName() << std::endl
               << std::endl;
 
@@ -22,13 +22,13 @@ i32 main()
 
     // transactions are used to deposit or withdraw money from the account
     // when the transaction object is created, the transaction is executed automatically
-    Bank::Transaction transaction1(1, &checking_account, 500, "Withdraw");
+    Bank::Transaction transaction1(1, checking_account, 500, "Withdraw");
     // display the transaciton details, and the account balance
     transaction1.Display();
 
     std::cout << std::endl;
 
-    Bank::Transaction transaction2(2, &checking_account, 1000, "Deposit");
+    Bank::Transaction transaction2(2, checking_account, 1000, "Deposit");
     transaction2.Display();
 
     return 0;
